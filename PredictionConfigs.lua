35d7861e8a35cadbb213b87fe71b764a3d2c5be1
marsvill <168480752+marsvill@users.noreply.github.com>
2025-04-02 01:30:33 -0600

    Create PredictionConfigs.lua

---
+++ PredictionConfigs.lua
@@ -0,0 +1,26 @@
+return {
+    {
+        name = "Auto Prediction 1",
+        predictions = {
+            [300] = 0.368788005255,
+            [290] = 0.35909146391,
+            [280] = 0.349394922565,
+            [270] = 0.33969838122,
+            [260] = 0.330001839875,
+            [250] = 0.32030529853,
+            -- ...existing prediction values...
+            [20] = 0.097285846595
+        }
+    },
+    {
+        name = "Auto Prediction 2", 
+        predictions = {
+            [200] = 0.2330000000,
+            [190] = 0.2230000000,
+            [180] = 0.2130000000,
+            -- ...existing prediction values...
+            [60] = 0.1229
+        }
+    },
+    -- Add more prediction configurations here following the same format
+}
